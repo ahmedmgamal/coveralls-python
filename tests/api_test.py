124e70af3c458491ccce42a1ef4c4b7eb78a62b4
ahmedmgamal <ahmed.gamal@ahmedgamal.info>
2019-04-26 17:04:50 -0400

    Update api_test.py

--- tests/api_test.py
+++ tests/api_test.py
@@ -5,7 +5,7 @@ from coveralls import Coveralls
 
 
 def test_output_to_file(tmpdir):
-    """Check we can write coveralls report into the file."""
+    """Check we can write coveralls report into the file. 1111"""
     test_log = tmpdir.join('test.log')
     Coveralls(repo_token='xxx').save_report(test_log.strpath)
     report = test_log.read()
